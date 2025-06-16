7fd6e7a0cfc6cbc57b1282888c5b20260ab4fc51
Christofer Nolander <christofer@nolander.me>
2025-06-16 12:58:55 +0200

    custom dvorak layout

---
+++ keyboards/zsa/moonlander/keymaps/nolanderc/keymap.c
@@ -0,0 +1,189 @@
+#include QMK_KEYBOARD_H
+#include "version.h"
+#define MOON_LED_LEVEL LED_LEVEL
+#define ML_SAFE_RANGE SAFE_RANGE
+
+enum custom_keycodes {
+    RGB_SLD = ML_SAFE_RANGE,
+    ST_MACRO_0,
+    ST_MACRO_1,
+    KC_ARING,
+    KC_ADOTS,
+    KC_ODOTS,
+};
+
+// clang-format off
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  [0] = LAYOUT_moonlander(
+    KC_LEFT_GUI,    KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_DELETE,      
+    KC_TAB,         KC_QUOTE,       KC_COMMA,       KC_DOT,         KC_P,           KC_Y,           LGUI(LCTL(KC_LEFT)),                            LGUI(LCTL(KC_RIGHT)),KC_F,      KC_G,           KC_C,           KC_R,           KC_L,           KC_SLASH,       
+    KC_ESCAPE,      KC_A,           KC_O,           KC_E,           KC_U,           KC_I,           LCTL(LSFT(KC_TAB)),                             LCTL(KC_TAB),   KC_D,           KC_H,           KC_T,           KC_N,           KC_S,           KC_MINUS,       
+    KC_LEFT_CTRL,   KC_SCLN,        KC_Q,           KC_J,           KC_K,           KC_X,                                                                           KC_B,           KC_M,           KC_W,           KC_V,           KC_Z,           KC_BSLS,        
+    KC_LEFT_CTRL,   KC_TRANSPARENT, KC_TRANSPARENT, KC_LEFT_ALT,    KC_LEFT_SHIFT,  ST_MACRO_0,                                                                     ST_MACRO_1,     KC_SPACE,       MO(3),          KC_RIGHT_ALT,   KC_TRANSPARENT, KC_RIGHT_CTRL,  
+                                                                    MO(2),          LGUI(LCTL(KC_LEFT)),TG(4),                                      RGB_VAI,        LGUI(LCTL(KC_RIGHT)),KC_ENTER
+  ),
+  [1] = LAYOUT_moonlander(
+    KC_EQUAL,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_LEFT,                                        KC_RIGHT,       KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       
+    KC_DELETE,      KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           TG(2),                                          TG(2),          KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,        
+    KC_BSPC,        KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_HYPR,                                        KC_MEH,         KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        MT(MOD_LGUI, KC_QUOTE),
+    KC_LEFT_SHIFT,  MT(MOD_LCTL, KC_Z),KC_X,        KC_C,           KC_V,           KC_B,                                                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         MT(MOD_RCTL, KC_SLASH),KC_RIGHT_SHIFT, 
+    LT(2, KC_GRAVE),CW_TOGG,        KC_TRANSPARENT, KC_LEFT,        KC_RIGHT,       MT(MOD_LALT, KC_APPLICATION),                                                   MT(MOD_LCTL, KC_ESCAPE),KC_UP,  KC_DOWN,        KC_LBRC,        KC_RBRC,        MO(2),          
+                                                                    KC_SPACE,       KC_BSPC,        KC_LEFT_GUI,                                    KC_LEFT_ALT,    KC_TAB,         KC_ENTER
+  ),
+  [2] = LAYOUT_moonlander(
+    KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_F11,                                         KC_F12,         KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_DELETE,      
+    KC_TRANSPARENT, KC_GRAVE,       KC_LABK,        KC_RABK,        KC_CIRC,        KC_CIRC,        KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_PIPE,        KC_AMPR,        KC_LBRC,        KC_RBRC,        KC_PERC,        KC_TILD,        
+    KC_TRANSPARENT, KC_EXLM,        KC_AT,          KC_HASH,        KC_DLR,         KC_PERC,        KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_ASTR,        KC_EQUAL,       KC_LPRN,        KC_RPRN,        LSFT(KC_S),     KC_MINUS,       
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,                                                                       KC_PLUS,        KC_PLUS,        KC_LCBR,        KC_RCBR,        KC_DLR,         KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
+                                                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_ENTER,       KC_BSPC
+  ),
+  [3] = LAYOUT_moonlander(
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_ARING,       KC_PGDN,        KC_UP,          KC_PAGE_UP,     KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_PGDN,        KC_PAGE_UP,     KC_RIGHT,       KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_ADOTS,       KC_LEFT,        KC_DOWN,        KC_RIGHT,       KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_ODOTS,       KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,                                                                       KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
+                                                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
+  ),
+  [4] = LAYOUT_moonlander(
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, 
+                                                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT
+  ),
+};
+// clang-format on
+
+extern rgb_config_t rgb_matrix_config;
+
+void keyboard_post_init_user(void) {
+    rgb_matrix_enable();
+}
+
+const uint8_t PROGMEM ledmap[][RGB_MATRIX_LED_COUNT][3] = {
+    [0] = {{0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {206, 218, 204}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {206, 218, 204}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {206, 218, 204}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {206, 218, 204}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {206, 218, 204}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {206, 218, 204}, {0, 245, 245}, {206, 218, 204}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245},
+           {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {206, 218, 204}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {206, 218, 204}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {206, 218, 204}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {206, 218, 204}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {206, 218, 204}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {206, 218, 204}, {0, 245, 245}, {206, 218, 204}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}, {0, 245, 245}},
+
+    [2] = {{21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {206, 218, 204}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {206, 218, 204}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {206, 218, 204}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {206, 218, 204}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {206, 218, 204}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {206, 218, 204}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245},
+           {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {206, 218, 204}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {206, 218, 204}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {206, 218, 204}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {206, 218, 204}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {206, 218, 204}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {206, 218, 204}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}, {21, 244, 245}},
+
+    [3] = {{206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204},
+           {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}, {206, 218, 204}},
+
+    [4] = {{144, 255, 167}, {144, 255, 167}, {34, 255, 255}, {144, 255, 167}, {144, 255, 167}, {144, 255, 167}, {144, 255, 167}, {34, 255, 255}, {144, 255, 167}, {144, 255, 167}, {144, 255, 167}, {144, 255, 167}, {34, 255, 255}, {144, 255, 167}, {144, 255, 167}, {34, 255, 255}, {34, 255, 255}, {34, 255, 255}, {34, 255, 255}, {34, 255, 255}, {144, 255, 167}, {144, 255, 167}, {34, 255, 255}, {144, 255, 167}, {144, 255, 167}, {144, 255, 167}, {144, 255, 167}, {34, 255, 255}, {144, 255, 167}, {144, 255, 167}, {144, 255, 167}, {34, 255, 255}, {144, 255, 167}, {144, 255, 167}, {144, 255, 167}, {144, 255, 167},
+           {144, 255, 167}, {144, 255, 167}, {34, 255, 255}, {144, 255, 167}, {144, 255, 167}, {144, 255, 167}, {144, 255, 167}, {34, 255, 255}, {144, 255, 167}, {144, 255, 167}, {144, 255, 167}, {144, 255, 167}, {34, 255, 255}, {144, 255, 167}, {144, 255, 167}, {34, 255, 255}, {34, 255, 255}, {34, 255, 255}, {34, 255, 255}, {34, 255, 255}, {144, 255, 167}, {144, 255, 167}, {34, 255, 255}, {144, 255, 167}, {144, 255, 167}, {144, 255, 167}, {144, 255, 167}, {34, 255, 255}, {144, 255, 167}, {144, 255, 167}, {144, 255, 167}, {34, 255, 255}, {144, 255, 167}, {144, 255, 167}, {144, 255, 167}, {144, 255, 167}},
+
+};
+
+void set_layer_color(int layer) {
+    for (int i = 0; i < RGB_MATRIX_LED_COUNT; i++) {
+        HSV hsv = {
+            .h = pgm_read_byte(&ledmap[layer][i][0]),
+            .s = pgm_read_byte(&ledmap[layer][i][1]),
+            .v = pgm_read_byte(&ledmap[layer][i][2]),
+        };
+        if (!hsv.h && !hsv.s && !hsv.v) {
+            rgb_matrix_set_color(i, 0, 0, 0);
+        } else {
+            RGB   rgb = hsv_to_rgb(hsv);
+            float f   = (float)rgb_matrix_config.hsv.v / UINT8_MAX;
+            rgb_matrix_set_color(i, f * rgb.r, f * rgb.g, f * rgb.b);
+        }
+    }
+}
+
+bool rgb_matrix_indicators_user(void) {
+    if (rawhid_state.rgb_control) {
+        return false;
+    }
+    if (keyboard_config.disable_layer_led) {
+        return false;
+    }
+    switch (biton32(layer_state)) {
+        case 0:
+            set_layer_color(0);
+            break;
+        case 2:
+            set_layer_color(2);
+            break;
+        case 3:
+            set_layer_color(3);
+            break;
+        case 4:
+            set_layer_color(4);
+            break;
+        default:
+            if (rgb_matrix_get_flags() == LED_FLAG_NONE) rgb_matrix_set_color_all(0, 0, 0);
+            break;
+    }
+    return true;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    int mods = get_mods();
+    switch (keycode) {
+        case ST_MACRO_0:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCTL(SS_TAP(X_INSERT)) SS_DELAY(100) SS_LCTL(SS_LSFT(SS_TAP(X_C))));
+            }
+            break;
+        case ST_MACRO_1:
+            if (record->event.pressed) {
+                SEND_STRING(SS_LCTL(SS_LSFT(SS_TAP(X_V))) SS_DELAY(100) SS_LSFT(SS_TAP(X_INSERT)));
+            }
+            break;
+
+        case RGB_SLD:
+            if (rawhid_state.rgb_control) {
+                return false;
+            }
+            if (record->event.pressed) {
+                rgblight_mode(1);
+            }
+            return false;
+
+        case KC_ARING: {
+            if (record->event.pressed) {
+                if (mods & MOD_MASK_SHIFT) {
+                    clear_mods();
+                    SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_1) SS_TAP(X_KP_9) SS_TAP(X_KP_7)));
+                    set_mods(mods);
+                } else {
+                    SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_2) SS_TAP(X_KP_9)));
+                }
+                return false;
+            }
+            break;
+        }
+        case KC_ADOTS: {
+            if (record->event.pressed) {
+                if (mods & MOD_MASK_SHIFT) {
+                    clear_mods();
+                    SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_1) SS_TAP(X_KP_9) SS_TAP(X_KP_6)));
+                    set_mods(mods);
+                } else {
+                    SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_2) SS_TAP(X_KP_8)));
+                }
+                return false;
+            }
+            break;
+        }
+        case KC_ODOTS: {
+            if (record->event.pressed) {
+                if (mods & MOD_MASK_SHIFT) {
+                    clear_mods();
+                    SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_1) SS_TAP(X_KP_4)));
+                    set_mods(mods);
+                } else {
+                    SEND_STRING(SS_LALT(SS_TAP(X_KP_0) SS_TAP(X_KP_2) SS_TAP(X_KP_4) SS_TAP(X_KP_6)));
+                }
+                return false;
+            }
+            break;
+        }
+    }
+    return true;
+}
